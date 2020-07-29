85d064f7400937d33585dbf4f1eabf875397ce46
Alexander Ho <alexhsz98@gmail.com>
2020-07-29 23:21:56 +0100

    First commit

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: "Login Demo App",
+      home: Scaffold(
+        body: Text("Form under constructions"),
+      ),
+    );
+  }
+}
