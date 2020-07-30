ad549b61913da94a7cbd49bbd0c61ff2a766cc58
Alexander Ho <alexhsz98@gmail.com>
2020-07-30 22:40:48 +0100

    Added stateful login screen class

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
     return MaterialApp(
       title: "Login Demo App",
       home: Scaffold(
-        body: Text("Form under constructions"),
+        body: LoginScreen(),
       ),
     );
   }
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            // passwordField(),
+            // submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "Email Address",
+        hintText: "example@example.com",
+      ),
+    );
+  }
+
+  Widget passwordField() {}
+
+  Widget submitButton() {}
+}
